0e0b4536b83a3266bad58da6e4352e3e5e7685e8
Luke Usher <flukus@gmail.com>
2012-02-14 12:43:42 +1100

    Fixed ActionBarIconLDPI constructor

--- src/org/fluent2d/resolutions/ActionBarIconLDPI.java
+++ src/org/fluent2d/resolutions/ActionBarIconLDPI.java
@@ -1,11 +1,11 @@
 package org.fluent2d.resolutions;
 
-import org.fluent2d.DrawableRegion;
+import org.fluent2d.Region;
 
 public class ActionBarIconLDPI extends Resolution {
 
 	public ActionBarIconLDPI() {
-		super(new DrawableRegion(0, 0, 18, 18), new DrawableRegion(4, 4, 14, 14),
+		super(new Region(0, 0, 18, 18), new Region(4, 4, 14, 14),
 				"drawable-ldpi");
 	}
 }
